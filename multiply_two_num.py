36a0c31a28887d2493ef0bb406d0465b81b693d5
Erin J Moore <16661530+rinnyssance@users.noreply.github.com>
2026-05-08 12:25:01 -0400

    Add program to multiply two numbers

---
+++ multiply_two_num.py
@@ -0,0 +1,16 @@
+def main():
+    print("This program multiplies two numbers.")
+
+    num1 = input("Enter first number: ")
+    num1 = int(num1)
+
+    num2 = input("Enter second number: ")
+    num2 = int(num2)
+
+    total = num1 * num2
+
+    print(total)
+
+
+if __name__ == '__main__':
+    main()
