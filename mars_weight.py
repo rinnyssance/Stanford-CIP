71921e3294bb42d9e910c645588aea2e245595a3
Erin J Moore <16661530+rinnyssance@users.noreply.github.com>
2026-05-08 08:44:06 -0400

    Expand weight calculation to include multiple planets
    Added calculations for weights on other planets including Mercury, Venus, Jupiter, Saturn, Uranus, and Neptune.

--- mars_weight.py
+++ mars_weight.py
@@ -1,15 +1,34 @@
 def main():
     # Create your variables
-    mars = 0.378
+    MERCURY = .376
+    MARS = 0.378
+    VENUS = .889
+    JUPITER = 2.36
+    SATURN = 1.081
+    URANUS = .815
+    NEPTUNE = 1.14
+
 
     # Ask for the weight on Earth
     weight = float(input("Enter a weight on Earth: "))
 
     # Calculate Mars weight
-    result = round(weight * mars, 2)
+    mercury_result = round(weight * MERCURY, 2)
+    mars_result = round(weight * MARS, 2)
+    venus_result = round(weight * VENUS, 2)
+    jupiter_result = round(weight * JUPITER, 2)
+    saturn_result = round(weight * SATURN, 2)
+    uranus_result = round(weight * URANUS)
+    neptune_result = round (weight * NEPTUNE)
 
     # Print result
-    print("The equivalent weight on Mars:", result)
-
+    print("The equivalent weight on Mercury: ", mercury_result)
+    print("The equivalent weight on Mars:", mars_result)
+    print("The equivalent weight on Venus:", venus_result)
+    print("The equivalent weight on Juputer:", juputer_result)
+    print("The equivalent weight on Saturn:", saturn_result)
+    print("The equivalent weight on Uranus:", uranus_result)
+    print("The equivalent weight on Neptune:", neptune_result)
+    
 if __name__ == "__main__":
     main()
