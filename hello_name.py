7b7f9b2a9ccd5cc71c929b7f2e2792bc19181624
Erin J Moore <16661530+rinnyssance@users.noreply.github.com>
2026-05-08 08:59:35 -0400

    Add hello_name.py to greet the user by name

---
+++ hello_name.py
@@ -0,0 +1,10 @@
+def main():
+    # Ask for user's name & create a variable
+    name = input("What is your name? ")
+
+    # Print the result
+    print("Hello", name)
+
+
+if __name__ == '__main__':
+    main()
